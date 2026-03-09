c995b48d80deea744a1a69466d25a38b472b5cbf
k_stein <karlasteinbrink@gmail.com>
2026-03-09 20:00:18 +0000

    Set up stripe integration

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51S8Ik3CNa0cNqqRjaxpVCrfzAZXZEu2vP6ixDmfRAMYJQYN1npWaxdYeIDa2yF1qt0Sc3Mg72t8WjpwWsqJZgmDC00Rf12bioh',  # noqa
+        'client_secret': 'test_client_secret'
     }
 
     return render(request, template, context)
