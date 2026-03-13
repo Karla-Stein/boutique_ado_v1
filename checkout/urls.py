97cf55caeffc7b0f99407bf9b973c602fc29c34e
k_stein <karlasteinbrink@gmail.com>
2026-03-13 14:43:07 +0000

    Map webhooks to custom handlers

--- checkout/urls.py
+++ checkout/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>/', views.checkout_success,
          name='checkout_success'),
-    path('wh/', webhook,
-         name='webhook'),
+    path('wh/', webhook, name='webhook'),
 ]
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -25,6 +25,7 @@ def webhook(request):
         event = stripe.Webhook.construct_event(
             payload, sig_header, settings.STRIPE_WH_SECRET
         )
+
     except ValueError as e:
         # Invalid payload
         return HttpResponse(content=e, status=400)
@@ -35,5 +36,26 @@ def webhook(request):
 
     except Exception as e:
         return HttpResponse(content=e, status=400)
-    
-   
+
+    print('Success!')
+    return HttpResponse(status=200)
+
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,  # noqa
+    }
+
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
