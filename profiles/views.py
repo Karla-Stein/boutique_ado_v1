197cd42427fcf0525970f43bc1a3b0bef3da97fb
k_stein <karlasteinbrink@gmail.com>
2026-03-16 14:10:11 +0000

    Start profile app and add urls and template

---
+++ profiles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def profile(request):
+    """
+    Dispay the users profile
+    """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(
+        request,
+        template,
+        context,
+    )
